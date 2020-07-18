8d99eef20b9d58573756928f6d55a3d2912db4fe
Yogi Prasetya <yogiprsetya@gmail.com>
2020-07-18 19:12:09 +0700

    Section 5, Lesson 1: add 3D object, implement Location

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 // This file contains the boilerplate to execute your React app.
 // If you want to modify your application's content, start in "index.js"
 
-import {ReactInstance} from 'react-360-web';
+import {ReactInstance, Location} from 'react-360-web';
 
 function init(bundle, parent, options = {}) {
   const r360 = new ReactInstance(bundle, parent, {
@@ -11,9 +11,9 @@ function init(bundle, parent, options = {}) {
   });
 
   // Render your app content to the default cylinder surface
-  r360.renderToSurface(
-    r360.createRoot('CryptoDashBoardVR', { /* initial props */ }),
-    r360.getDefaultSurface()
+  r360.renderToLocation(
+    r360.createRoot('CryptoModel', { /* initial props */ }),
+    new Location([0,0,-1])
   );
 
   // Load the initial environment
--- index.js
+++ index.js
@@ -1,43 +1,28 @@
 import React from 'react';
 import {
+  asset,
   AppRegistry,
   StyleSheet,
   Text,
   View,
 } from 'react-360';
+import Entity from 'Entity';
 
-export default class CryptoDashBoardVR extends React.Component {
+export default class CryptoModel extends React.Component {
   render() {
     return (
-      <View style={styles.panel}>
-        <View style={styles.greetingBox}>
-          <Text style={styles.greeting}>
-            Welcome to React 360
-          </Text>
-        </View>
+      <View>
+        <Entity
+          style={{transform: [{scaleX: 1}, {scaleY: 1}, {scaleZ: 1}, {rotateX: 90}]}}
+          source={{obj: asset('models/BTC.obj')}}
+        />
       </View>
     );
   }
 };
 
 const styles = StyleSheet.create({
-  panel: {
-    // Fill the entire surface
-    width: 1000,
-    height: 600,
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  greetingBox: {
-    padding: 20,
-    backgroundColor: '#000000',
-    borderColor: '#639dda',
-    borderWidth: 2,
-  },
-  greeting: {
-    fontSize: 30,
-  },
+
 });
 
-AppRegistry.registerComponent('CryptoDashBoardVR', () => CryptoDashBoardVR);
+AppRegistry.registerComponent('CryptoModel', () => CryptoModel);
